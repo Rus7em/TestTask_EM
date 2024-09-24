f15df06e92a8a43a939c09897ca77da60c8ff263
tippy <tippy@mail.com>
2024-09-24 19:05:12 +0300

    add read schemes

--- src/schemas/order.py
+++ src/schemas/order.py
@@ -1,9 +1,20 @@
 from pydantic import BaseModel
 from typing import List
+from datetime import datetime
 
-from order_item import CreateOrderItem
+from order_item import CreateOrderItem, ReadOrderItem
 
 
 class CreateOrder(BaseModel):
     state: str
     items: List[CreateOrderItem]
+
+
+class ReadOrder(BaseModel):
+    id: int
+    create_at: datetime
+    status_id: int
+    items: List[ReadOrderItem]
+
+    class Config:
+        orm_mode = True
--- src/schemas/order_item.py
+++ src/schemas/order_item.py
@@ -4,3 +4,10 @@ from pydantic import BaseModel
 class CreateOrderItem(BaseModel):
     product_id: int
     num: int
+
+
+class ReadOrderItem(BaseModel):
+    id: int
+    order_id: int
+    product_id: int
+    num: int
