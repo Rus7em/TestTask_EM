f5c68c49b86b86dde4e91ef09c58dc7a31e669f8
tippy <tippy@mail.com>
2024-09-24 14:21:23 +0300

    product.price int to float

--- src/model/product.py
+++ src/model/product.py
@@ -1,6 +1,6 @@
 from typing import Optional
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String
+from sqlalchemy import String, Float
 
 from database import Base
 
@@ -11,5 +11,5 @@ class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(50))
     description: Mapped[Optional[str]]
-    price: Mapped[Optional[int]]
+    price: Mapped[Optional[float]] = mapped_column()
     num: Mapped[int]
