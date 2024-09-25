09dd888ea6d3db405388b9e6d89bf05e03c1a414
tippy <tippy@mail.com>
2024-09-25 14:25:20 +0300

    update api

--- src/api/v1/product.py
+++ src/api/v1/product.py
@@ -1,25 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Body, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import List, Annotated
 
+from schemas.product import CreateProduct, ReadProduct
+import service
+from database import get_db
 
 router = APIRouter()
 
+async def product_parameters(name: str, description: str, price: str, num: int):
+    return {"name": name, "description": description, "price": price, "num": num}
 
-@router.post("/")
-def add_products():
-    pass
+product_dep = Annotated[dict, Depends(product_parameters)]
 
-@router.get("/")
-def get_product_list():
-    pass
 
-@router.get("/{id}")
-def get_product():
-    pass
+@router.post("/", response_model=ReadProduct)
+async def add(parameters: product_dep, db: AsyncSession = Depends(get_db)):
+    product = CreateProduct(**parameters)
+    return await service.add_product(product=product, db=db)
 
-@router.put("/{id}")
-def edit_product():
-    pass
+@router.get("/", response_model=List[ReadProduct])
+async def get_list(db: AsyncSession = Depends(get_db)):
+    return await service.get_product_list(db=db)
+
+@router.get("/{id}", response_model=ReadProduct)
+async def get(id: int, db: AsyncSession = Depends(get_db)):
+    return await service.get_product(id=id, db=db)
+
+@router.put("/{id}", response_model=ReadProduct)
+async def edit(id: int, parameters: product_dep, db: AsyncSession = Depends(get_db)):
+    product = CreateProduct(**parameters)
+    return await service.edit_product(id=id, product=product, db=db)
 
 @router.delete('/{id}')
-def delete_product():
-    pass
+async def delete(id: int, db: AsyncSession = Depends(get_db)):
+    return await service.delete_product(id=id, db=db)
