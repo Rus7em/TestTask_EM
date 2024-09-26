e6c0e8b8d83ec66682311f5c0e4fdc99274593cc
tippy <tippy@mail.com>
2024-09-26 12:23:56 +0300

    clear main file

--- src/main.py
+++ src/main.py
@@ -1,15 +1,10 @@
 from fastapi import FastAPI
-import asyncio
 
 from api.v1.order import router as order_router
 from api.v1.product import router as product_router
 
-from database import engine, Base
 
 app = FastAPI()
-# app.include_router(order_router, prefix="/orders", tags=["Orders"])
+
+app.include_router(order_router, prefix="/orders", tags=["Orders"])
 app.include_router(product_router, prefix="/products", tags=["Producs"])
-
-
-
-
