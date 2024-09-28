4955c92adbf957df4500975faf26241a0cfd3781
tippy <tippy@mail.com>
2024-09-28 13:10:11 +0300

    fix import declarative_base

--- src/database.py
+++ src/database.py
@@ -1,6 +1,5 @@
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import declarative_base
 
 import config
 
