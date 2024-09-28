dd6ade4713989af61e33cdfdc9e7ca5937b081f3
tippy <tippy@mail.com>
2024-09-28 12:03:05 +0300

    fix alembic - without status

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from database import Base
-from models import order, order_item, product, status
+from models import order, order_item, product
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
